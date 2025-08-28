7a40d1294d2bc50a7982fe55624feaa542addc88
Taukir Ahmed <ataukir30@gmail.com>
2025-08-28 16:07:40 +0600

    copy function done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//like counter functionality--------
 let likes = document.getElementsByClassName('like');
 let likeClick = 0;
 for( let like of likes ){
@@ -6,4 +7,28 @@ for( let like of likes ){
         likeClick++ ;
         document.getElementById('like_counter').innerText = likeClick;
     })
+}
+
+//copy counter function--------
+let copyCount = 0;
+
+function copyToClipboard(id){
+    const textToCopy = document.getElementById(id).innerText;
+    // console.log(textToCopy)
+
+    //calling clipboard API for copying
+    navigator.clipboard.writeText(textToCopy).then( () => {
+        console.log('Text copied', textToCopy);
+        document.getElementById('msg').style.display = 'block';
+
+        alert('Number Copied !');
+
+        setTimeout(() => {
+            document.getElementById('msg').style.display = 'none';
+        }, 2000);
+    } )
+
+    //increasing copy count----
+    copyCount++;
+    document.getElementById('copy_counter').innerText = copyCount;
 }
