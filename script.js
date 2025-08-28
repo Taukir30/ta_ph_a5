8ae525bd3e039b0b2bea080d419acf28872255b9
Taukir Ahmed <ataukir30@gmail.com>
2025-08-28 21:57:17 +0600

    calling functionality done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let histories = [];  //history array--------
+
 //like counter functionality--------
 let likes = document.getElementsByClassName('like');
 let likeClick = 0;
@@ -34,30 +36,61 @@ function copyToClipboard(id){
 }
 
 
+//calling function-----------
+function makeCall(id1, id2){
 
+    let coins = parseInt( document.getElementById('coins').innerText );
+    let callingName = document.getElementById(id1).innerText;
+    let callingNumber = document.getElementById(id2).innerText;
 
-//history section dynamic functionality----------
-let histories = [
-    {name:'Fire Service Number', number:999, time:'11:36:58 AM'},
-    {name:'Fire Service Number', number:999, time:'11:36:58 AM'}
-];
+    // console.log(callingNumber)
+
+    if( coins < 20 ){
+        alert('Unable to make the call. \nNot enough coins to make the call');
+        return;
+    }
+
+    alert( `Now Calling: ${callingName} \nNumber: ${callingNumber}` );
+    coins -= 20;
+    document.getElementById('coins').innerText = coins;
+
+    let now = new Date();
+    let time = now.toLocaleTimeString();
+    let lastCall = {name:callingName, number:callingNumber, time:time};
+
+    histories.push(lastCall);
+
+    printHistory();
+
+}
 
 let historySection = document.getElementById('history');
 
-for( let history of histories ){
 
-    let tile = document.createElement('div');
-    tile.className = "tile bg-[#FAFAFA] rounded-md p-2 m-2 shadow-md flex justify-between items-center";
 
-    tile.innerHTML = `<div class="left">
-                        <h4 class="inter font-semibold text-xs">${history.name}</h4>
-                        <p class="text-xs text-gray-500 my-1">${history.number}</p>
-                    </div>
-                    <span class="text-xs">${history.time}</span>`
-    
-    historySection.appendChild(tile);
+//history section dynamic functionality----------
+function printHistory(){
+
+    historySection.innerHTML = "";
+
+    for( let history of histories ){
+
+        let tile = document.createElement('div');
+        tile.className = "tile bg-[#FAFAFA] rounded-md p-2 m-2 shadow-md flex justify-between items-center";
+
+        tile.innerHTML = `<div class="left">
+                            <h4 class="inter font-semibold text-xs">${history.name}</h4>
+                            <p class="text-xs text-gray-500 my-1">${history.number}</p>
+                        </div>
+                        <span class="text-xs">${history.time}</span>`
+        
+        historySection.appendChild(tile);
+
+    }
 
 }
+
+printHistory();
 
 //clear history fuction-----
 document.getElementById('btn_clear').addEventListener('click', function(e){
